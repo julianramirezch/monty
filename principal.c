702de607242282367ef7e54f3dd45a9e5b5b5550
julianramirezch <julianramirezch1@gmail.com>
2020-05-13 15:02:43 -0500

    [refactor]

--- principal.c
+++ principal.c
@@ -4,7 +4,7 @@
  * principal - Function principal of the project
  * @var: Global struct
  * @head: Pointer to head struct stack_s
- * Return: 0 on succes, 1 in failure
+ * Return: 0 on succes, -1 in failure
  */
 
 int principal(st_va *var, stack_t **head)
@@ -14,13 +14,20 @@ int principal(st_va *var, stack_t **head)
 
 	var->head = head;
 	token = tokens(var);
-	if (token != 1)
+	if (token == 0)
+	{
 		get = get_func(var);
-	else
+		if (get == -1)
+			return (-1);
+	}
+
+	if (token == 1)
 		return (1);
 
-	if (get == 1)
+	if (token == -1)
+	{
 		return (-1);
+	}
 
 	return (0);
 }
