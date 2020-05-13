50786f254c05eedecde8b753abe796912085f112
julianramirezch <julianramirezch1@gmail.com>
2020-05-13 16:29:49 -0500

    [refactor]

--- f_push.c
+++ f_push.c
@@ -8,7 +8,7 @@
 void push(st_va *var)
 {
 	/* add_dnodeint(var->head, var->number, var);*/
-	stack_t *newn, *aux;
+	stack_t *newn;
 
 	newn = malloc(sizeof(stack_t));
 	if (newn == NULL)
@@ -20,18 +20,14 @@ void push(st_va *var)
 	}
 	newn->n = var->number;
 	newn->prev = NULL;
-	newn->next = NULL;
 
 	if (*var->head == NULL)
-	{
-		*var->head = newn;
-		(*var->head)->next = NULL;
-		(*var->head)->prev = NULL;
-		return;
-	}
+		newn->next = NULL;
+	else
+		newn->next = *var->head;
 
-	aux = *var->head;
-	newn->next = aux;
-	aux->prev = newn;
+	if (newn->next != NULL)
+		newn->next->prev = newn;
+
 	*var->head = newn;
 }
